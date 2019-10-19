eb9456c725223a29ed0a7cde48e426687bf6a303
Piotr Kapski <kapski.p@gmail.com>
2019-10-19 18:34:00 +0200

    Fixed CORS

---
+++ services/app/src/main/java/pl/polsl/app/configuration/WebConfig.java
@@ -0,0 +1,30 @@
+package pl.polsl.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
+
+@Configuration
+@ComponentScan({"pl.polsl"})
+public class WebConfig {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurerAdapter() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**");
+                }
+            };
+        }
+}
