f298f271cb2b30015431c38de6a2a27ed1d78fdd
Piotr Kapski <kapski.p@gmail.com>
2019-11-01 21:33:04 +0100

    Fixed cors for delete

--- services/app/src/main/java/pl/polsl/app/configuration/WebConfig.java
+++ services/app/src/main/java/pl/polsl/app/configuration/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig {
             return new WebMvcConfigurerAdapter() {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**");
+                    registry.addMapping("/**").allowedOrigins("*").allowedMethods("HEAD","GET", "POST","PUT", "DELETE","PATCH","OPTIONS");
                 }
             };
         }
